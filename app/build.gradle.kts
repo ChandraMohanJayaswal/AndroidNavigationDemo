0e275bbc7cbbce8d52ac472d642f6f71e000affa
sunil bist <sam.bist7686@gmail.com>
2024-08-27 14:22:54 +0545

    navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // navigation dependencies
+    implementation ("androidx.navigation:navigation-compose:2.6.0")
+    implementation ("androidx.compose.material:material:1.5.0") // Use the latest version
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.5.0")
+    implementation ("androidx.compose.ui:ui:1.5.0")
 }
--- app/src/main/java/com/example/logindemoapp/ui/view/ScreenHome.kt
+++ app/src/main/java/com/example/logindemoapp/ui/view/ScreenHome.kt
@@ -3,11 +3,24 @@ package com.example.logindemoapp.ui.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.logindemoapp.R
 import com.example.logindemoapp.ui.theme.LoginDemoAppTheme
 
 @Composable
@@ -15,8 +28,39 @@ fun ScreenHomeView(){
     Column(modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "you are in Home")
+        BottomNavigationSample()
 
+    }
+}
+@Composable
+fun BottomNavigationSample(
+){
+    val context = LocalContext.current
+    var seletedItem by remember { mutableStateOf(0)}
+    val items = listOf(
+        context.getString(R.string.item1),
+        context.getString(R.string.item2),
+        context.getString(R.string.item3)
+    )
+
+
+
+    Scaffold (
+        bottomBar = {
+            BottomNavigation (modifier = Modifier.navigationBarsPadding()){
+                items.forEachIndexed{index,item ->
+                    BottomNavigationItem(
+                        selected =seletedItem==index ,
+                        onClick = {seletedItem = index },
+                        icon = {
+                            Icon(Icons.Filled.Favorite, contentDescription ="fav" )
+                        },
+                        label = { Text(text = "$item")})
+                }
+            }
+        }
+    ){ innerPadding ->
+        // Your screen content goes here, with the padding applied
     }
 }
 @Preview(showBackground = true)
