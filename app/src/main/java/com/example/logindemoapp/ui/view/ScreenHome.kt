dbcc43f2f7dffb97b4716bec98d020031228bf16
Chandra Mohan Jayaswal <chandra.ets@gmail.com>
2024-08-28 13:45:21 +0545

    Update

--- app/src/main/java/com/example/logindemoapp/ui/view/ScreenHome.kt
+++ app/src/main/java/com/example/logindemoapp/ui/view/ScreenHome.kt
@@ -11,39 +11,30 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.logindemoapp.ui.theme.LoginDemoAppTheme
-import com.example.logindemoapp.ui.view.navview.BottomNavItems
 import com.example.logindemoapp.ui.view.navview.NavCoordinator
-import com.example.logindemoapp.ui.view.navview.ScreenNavActivitiesView
-import com.example.logindemoapp.ui.view.navview.ScreenNavGroupsView
-import com.example.logindemoapp.ui.view.navview.ScreenNavHomeView
-import com.example.logindemoapp.ui.view.navview.ScreenNavResourcesView
-import com.example.logindemoapp.ui.view.navview.ScreenNavSkillView
-import com.example.logindemoapp.ui.view.navview.getNavItems
 
 
 @Composable
 fun ScreenHomeView(
     logOutAction: () -> Unit
 ) {
-    val navitems = getNavItems()
     val navController = rememberNavController()
     val coordinator: NavCoordinator = remember { NavCoordinator(navController) }
 
     Scaffold(
         bottomBar = {
-            BottomNavigationBar(navController,coordinator, navitems)
+            BottomBar(navController,coordinator)
         },
-        topBar = {TopApplicationBar(logOutAction = logOutAction)}
+        topBar = {
+            TopBar(logOutAction = logOutAction)
+        }
     ) { innerPadding ->
         coordinator.NavigationHost(innerPadding = innerPadding, startDestination = "home_screen")
     }
@@ -51,7 +42,7 @@ fun ScreenHomeView(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopApplicationBar(
+fun TopBar(
     logOutAction : (()->Unit)) {
         TopAppBar(
             title = {
@@ -68,14 +59,13 @@ fun TopApplicationBar(
 }
 
 @Composable
-fun BottomNavigationBar(
+fun BottomBar(
     navController: NavController,
-   coordinator: NavCoordinator,
-    navitems: List<BottomNavItems>) {
+    coordinator: NavCoordinator) {
     NavigationBar(
         modifier = Modifier.navigationBarsPadding()
     ) {
-        navitems.forEachIndexed { index, item ->
+        coordinator.getNavItems().forEachIndexed { index, item ->
             val isSelected = navController.currentBackStackEntryAsState().value?.destination?.route == item.route
             NavigationBarItem(
                 selected = isSelected,
--- app/src/main/java/com/example/logindemoapp/ui/view/navview/NavCoordinator.kt
+++ app/src/main/java/com/example/logindemoapp/ui/view/navview/NavCoordinator.kt
@@ -1,6 +1,13 @@
 package com.example.logindemoapp.ui.view.navview
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -8,9 +15,11 @@ import androidx.navigation.compose.composable
 interface NavCoordinatorInterface{
    fun nagivateTo(route : String)
 }
+
+
 class NavCoordinator(
     val navController: NavHostController
-)  : NavCoordinatorInterface{
+)  : NavCoordinatorInterface {
 
     override fun nagivateTo(route: String) {
         navController.navigate(route) {
@@ -51,4 +60,23 @@ class NavCoordinator(
             }
         }
     }
+
+
+    fun getNavItems() : List<BottomNavItems>{
+        return listOf(
+            BottomNavItems("Home", Icons.Filled.Home,"home_screen"),
+            BottomNavItems("Activities", Icons.Filled.AddCircle,"activity_screen"),
+            BottomNavItems("Skill", Icons.Filled.AccountCircle,"skill_screen"),
+            BottomNavItems("Groups", Icons.Filled.CheckCircle,"group_screen"),
+            BottomNavItems("Resources", Icons.Filled.Info,"resources_screen")
+        )
+    }
+
 }
+
+
+data class BottomNavItems (
+    val label : String,
+    val icon : ImageVector,
+    val route : String
+)
